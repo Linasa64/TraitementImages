e8c24aad5c225118aff8ed9df6797fead89975b6
Obain <86167470+Obain@users.noreply.github.com>
2021-09-10 10:42:54 +0200

    Update testimages2.py

--- testimages2.py
+++ testimages2.py
@@ -7,6 +7,13 @@ Created on Fri Sep 10 10:03:17 2021
 
 from skimage import io
 
+# on importe l'image et on le transforme en tableau numpy
 img = io.imread("image2_reference.png")
+
+for i in range (img.shape[0]):
+    
+    img[i,] = 0
+
+# afficher l'image
 io.imshow(img)
 io.show
