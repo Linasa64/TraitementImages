1f2b2b6c1cd0ef9c04e0ace9159930a3cef4b829
linasa <lina.b4007@gmail.com>
2021-09-10 11:03:44 +0200

    Salt and pepper ok

--- testImages.py
+++ testImages.py
@@ -25,14 +25,23 @@ Created on Fri Sep 10 09:43:15 2021
 # cv.destroyAllWindows()
 
 from skimage import io
+from random import randint, random
 
 img = io.imread("image2_reference.png")
 
 print (img.shape)
 print (img[0,0])
 
-for i in range(0,img.shape[0]):
-    img[i,100] = 255
-    
+# for i in range(img.shape[0]):
+#     img[i,100] = 0
+
+for line in range(len(img)):
+    for col in range(len(img)):
+        if (randint(1, 10) == 1):
+            if (random() <0.5):
+                img[line, col] = 0
+            else:
+                img[line, col] = 255
+
 io.imshow(img)
 io.show
