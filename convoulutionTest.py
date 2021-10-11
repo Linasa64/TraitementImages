c7340eb2fa9f5fad242e1acdecbac7b147334546
linasa <lina.b4007@gmail.com>
2021-10-11 11:50:43 +0200

    testDebruitageLi
    ok avec moyenne

--- convoulutionTest.py
+++ convoulutionTest.py
@@ -5,35 +5,111 @@ Created on Mon Oct  4 12:04:00 2021
 @author: aubin
 """
 
+import matplotlib.cm as cm
 import numpy as np
 from skimage import io
+import matplotlib.pyplot as plt
+
 
 img = io.imread("image1_bruitee_snr_10.8656.png")
 
 def filtreRehausseur(img):
-    rehausseur = ([[0, -1, 0],
-                   [-1, 5, -1],
-                   [0, -1, 0]])
+    rehausseurM = ([[1/9, 1/9, 1/9],
+                   [1/9, 1/9, 1/9],
+                   [1/9, 1/9, 1/9]])
+    
+    # rehausseurGaussien = ([[1/16, 1/8, 1/16],
+    #                        [1/8, 1/4, 1/8],
+    #                        [1/16, 1/8, 1/16]])
+    
+    # rehausseurEdgeDetector = ([[-1, -1, -1],
+    #                            [-1, 8, -1],
+    #                            [-1, -1, -1]])
 
-    for line in range(0, len(img)):
-        for col in range(0, len(img)):
+    for line in range(1, len(img)-1):
+        for col in range(1, len(img)-1):
                 sum = 0
                 for line2 in range(0, 3):
                     for col2 in range(0, 3):
-                        ptm = pixelToMatrice(line2, col2)
-                        sum += ptm[line2, col2]*rehausseur[line2, col2]
+                        #print("line : ", line, " / col : ", col)
+                        ptm = pixelToMatrice(line, col)
+                        sum += ptm[line2][col2]*rehausseurM[line2][col2]
                 img[line, col] = sum
 
 def pixelToMatrice(x, y):
     entourage = np.array([[0, 0, 0],
                           [0, 0, 0],
                           [0, 0, 0]])
-    for line in range((x-1), (x+2)):
-        for col in range((y-1), (y+2)):
-            entourage[line, col] = img[line, col]
+    indX = x-1
+    indY = y-1
+    matX = 0
+    matY = 0
+    
+    while(indX<=x+1):
+        while(indY<=y+1):
+            #print("indX :", indX, " / indY :", indY, " / matX: ", matX, " / matY :", matY)
+            entourage[matX][matY] = img[indX, indY]
+            indY+=1
+            matY+=1
+        indX+=1
+        matX+=1
+        indY=y-1
+        matY=0
     return entourage
             
-#Affichage
+
+
+
 filtreRehausseur(img)
-io.imshow(img)
-io.show
+
+
+## CALCUL SNR
+
+from math import log, log10
+
+imgRef = io.imread("image1_reference.png")
+imgBruit = img
+
+pSignal = 0
+pBruit = 0
+
+for line in range(0, len(imgBruit)):
+    for col in range(0, len(imgBruit)):
+        pSignal = pSignal + int(imgRef[line, col])**2
+        pBruit = pBruit + (int(imgBruit[line, col])-int(imgRef[line, col]))**2
+ 
+
+print("pSignal : ", pSignal)
+print("pBruit : ", pBruit)
+snr = 10*log((pSignal/pBruit), 10)
+print("SNR: ", snr)
+
+#Affichage
+# io.imshow(img)
+img = io.imread("image1_bruitee_snr_10.8656.png")
+fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(10, 300))
+axes[0].imshow(img, cmap=cm.gray)
+axes[0].set_title('Bruitée')
+axes[1].imshow(imgBruit, cmap=cm.gray)
+axes[1].set_title('Débruitée')
+axes[2].imshow(imgRef, cmap=cm.gray)
+axes[2].set_title('Originale')
+
+io.show   
+
+
+
+#Affichage
+
+
+imgRef = io.imread("image1_reference.png")
+imgBruit = io.imread("image1_bruitee_snr_10.8656.png")
+fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(10, 300))
+axes[0].imshow(imgBruit, cmap=cm.gray)
+axes[0].set_title('Bruitée')
+axes[1].imshow(img, cmap=cm.gray)
+axes[1].set_title('Débruitée')
+axes[2].imshow(imgRef, cmap=cm.gray)
+axes[2].set_title('Originale')
+
+io.show   
