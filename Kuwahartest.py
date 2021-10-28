23c1479b1103bb7d17b7c75dc5edada531bdccbb
linasa <lina.b4007@gmail.com>
2021-10-28 09:34:29 +0200

    Kuwa OK

--- Kuwahartest.py
+++ Kuwahartest.py
@@ -1,43 +1,82 @@
 import matplotlib.cm as cm
 from skimage import io
 import matplotlib.pyplot as plt
-from PIL import Image
+import math
+import statistics
+import numpy as np
 
 #Affichage
 
-img = Image.open("squirrel.jpg")
-imgKuwa = img
+img = io.imread("image1_bruitee_snr_10.8656.png")
+       
+       
 
-print()
-print(img.size);
+def moyenneEnFonctionEcartTypeMin(a, b, c, d):
+    listeEcartsTypes = [np.nanstd(a), np.nanstd(b), np.nanstd(c), np.nanstd(d)]
+    posMin = listeEcartsTypes.index(min(listeEcartsTypes))
+    
+    if(posMin == 0):
+        return a.mean()
+    elif (posMin == 1):
+        return b.mean()
+    elif (posMin == 2):
+        return c.mean()
+    else:
+        return d.mean()
+     
 
-for line in range(0, 396) :
-   for col in range(0, 500) :
+for line in range(2, len(img)-2) :
+   for col in range(2, len(img)-2) :
             
-       a = ([[img[line-2, col-2], img[line-2, col-1], img[line-2, col]],
+       a = np.array([[img[line-2, col-2], img[line-2, col-1], img[line-2, col]],
              [img[line-1, col-2], img[line-1, col-1], img[line-1, col]], 
              [img[line, col-2], img[line, col-1], img[line, col]]])
             
-       b = ([[img[line-2, col+2], img[line-2, col+1], img[line-2, col]],
+       b = np.array([[img[line-2, col+2], img[line-2, col+1], img[line-2, col]],
              [img[line-1, col+2], img[line-1, col+1], img[line-1, col]],
              [img[line, col+2], img[line, col+1], img[line, col]]])
             
-       c = ([[img[line, col-2], img[line, col-1], img[line, col]],
+       c = np.array([[img[line, col-2], img[line, col-1], img[line, col]],
             [img[line+1, col-2], img[line+1, col-1], img[line+1, col]],
             [img[line+2, col-2], img[line+2, col-1], img[line+2, col]]])
             
-       d = ([[img[line, col+2], img[line, col+1], img[line, col]],
+       d = np.array([[img[line, col+2], img[line, col+1], img[line, col]],
             [img[line+1, col+2], img[line+1, col+1], img[line+1, col]],
             [img[line+2, col+2], img[line+2, col+1], img[line+2, col]]])
-
-       a2 = a[0]+a[1]+a[2]; 
        
-print(a2);
-    
-fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 7))
-axes[0].imshow(imgKuwa, cmap=cm.gray)
-axes[0].set_title('Squirrrrrrrel')
-axes[1].imshow(img, cmap=cm.gray)
-axes[1].set_title('Kuwahara')
+       img[line, col] = moyenneEnFonctionEcartTypeMin(a, b, c, d)
 
-io.show 
+## CALCUL SNR
+
+from math import log, log10
+
+imgRef = io.imread("image1_reference.png")
+imgBruit = img
+
+pSignal = 0
+pBruit = 0
+
+for line in range(0, len(imgBruit)):
+    for col in range(0, len(imgBruit)):
+        pSignal = pSignal + int(imgRef[line, col])**2
+        pBruit = pBruit + (int(imgBruit[line, col])-int(imgRef[line, col]))**2
+ 
+
+print("pSignal : ", pSignal)
+print("pBruit : ", pBruit)
+snr = 10*log((pSignal/pBruit), 10)
+print("SNR: ", snr)
+
+
+#Affichage
+# io.imshow(img)
+imgRefBruit = io.imread("image1_bruitee_snr_10.8656.png")
+fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(10, 300))
+axes[0].imshow(imgRefBruit, cmap=cm.gray)
+axes[0].set_title('Bruitée')
+axes[1].imshow(img, cmap=cm.gray)
+axes[1].set_title('Débruitée')
+axes[2].imshow(imgRef, cmap=cm.gray)
+axes[2].set_title('Originale')
+
+io.show   
